03efba39ad8ff081425afb6f34d2edbaa1601b1e
jordan hurst <jordanhurst@gmail.com>
2020-10-28 23:08:25 -0500

    * .gitignore: finished - no bonus
    * HelloASPDotNet.sln:
    * Startup.cs:
    * Program.cs:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * HelloASPDotNet.csproj:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * HelloController.cs:
    * launchSettings.json:
    * LICENSE.txt:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap.bundle.js:
    * bootstrap-grid.css:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap.bundle.min.js.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloASPDotNet.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/'>" +
+                "<input type='text' name='name' />" +
+                "<select name='language'>" +
+                    "<option value='english'>English</option>" +
+                    "<option value='french'>French</option>" +
+                    "<option value='spanish'>Spanish</option>" +
+                    "<option value='german'>German</option>" +
+                    "<option value='latin'>Latin</option>" +
+                "<input type='submit' value='Say Hi' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        // Post: /helloworld/
+        //[HttpGet("/welcome/{name?}")]
+        //[HttpPost]
+        //public IActionResult Welcome(string name = "World")
+        //{
+        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        //}
+
+        //Post: /helloWorld/
+        [HttpGet("/welcome/{name?}/{language?}")]
+        [HttpPost]
+        public IActionResult CreateMessage(string name = "World", string language = "english")
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                name = "World";
+            }
+            if (language == "french")
+            {
+                return Content("<h1>Bonjour, " + name + "!</h1>", "text/html");
+            }
+
+            else if (language == "spanish")
+            {
+                return Content("<h1>Hola, " + name + "!</h1>", "text/html");
+            }
+
+            else if (language == "german")
+            {
+                return Content("<h1>Hallo, " + name + "!</h1>", "text/html");
+            }
+
+            else if (language == "latin")
+            {
+                return Content("<h1>Salve, " + name + "!</h1>", "text/html");
+            }
+
+            else
+            {
+                return Content("<h1>Hello, " + name + "!</h1>", "text/html");
+            }
+        }
+    }
+}
