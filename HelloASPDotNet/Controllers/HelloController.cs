1096c97ddca0e27d23405ab9380183ef4affec44
jordan hurst <jordanhurst@gmail.com>
2020-11-10 22:16:11 -0600

    using Views and Razor

--- HelloASPDotNet/Controllers/HelloController.cs
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -8,67 +8,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelloASPDotNet.Controllers
 {
-    [Route("/helloworld")]
     public class HelloController : Controller
     {
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" +
-                "<input type='text' name='name' />" +
-                "<select name='language'>" +
-                    "<option value='english'>English</option>" +
-                    "<option value='french'>French</option>" +
-                    "<option value='spanish'>Spanish</option>" +
-                    "<option value='german'>German</option>" +
-                    "<option value='latin'>Latin</option>" +
-                "<input type='submit' value='Say Hi' />" +
-                "</form>";
-            return Content(html, "text/html");
+            return View();
         }
 
-        // Post: /helloworld/
-        //[HttpGet("/welcome/{name?}")]
-        //[HttpPost]
-        //public IActionResult Welcome(string name = "World")
-        //{
-        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-        //}
-
-        //Post: /helloWorld/
-        [HttpGet("/welcome/{name?}/{language?}")]
         [HttpPost]
-        public IActionResult CreateMessage(string name = "World", string language = "english")
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
         {
-            if (String.IsNullOrEmpty(name))
-            {
-                name = "World";
-            }
-            if (language == "french")
-            {
-                return Content("<h1>Bonjour, " + name + "!</h1>", "text/html");
-            }
-
-            else if (language == "spanish")
-            {
-                return Content("<h1>Hola, " + name + "!</h1>", "text/html");
-            }
-
-            else if (language == "german")
-            {
-                return Content("<h1>Hallo, " + name + "!</h1>", "text/html");
-            }
-
-            else if (language == "latin")
-            {
-                return Content("<h1>Salve, " + name + "!</h1>", "text/html");
-            }
-
-            else
-            {
-                return Content("<h1>Hello, " + name + "!</h1>", "text/html");
-            }
+            ViewBag.person = name;
+            return View();
         }
     }
 }
